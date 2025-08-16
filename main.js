13c056e3255db3995c6f95b142e1b6a6439d4ec9
SindorKholikberdiev <sindorkholikberdiev@gmail.com>
2025-08-16 14:34:48 +0500

    Feat: Add JS array drills and dynamically render skills list

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ projectCardBtn.forEach((btn) => {
   });
 });
 
-//Formaga ma'lumot kiritilib, "Send" tugmasi bosilganda, o'sha kiritilgan ma'lumotlarni o'qib olib consolega chiqarishni tushuntirish
+//Formaga ma'lumot kiritilib, "Send" tugmasi bosilganda, o'sha kiritilgan ma'lumotlarni o'qib olib consolega chiqarishni
 const form = document.querySelector("form"); // formani olish
 
 // formani submit hodisasini tinglash
@@ -37,7 +37,7 @@ form.addEventListener("submit", (event) => {
   });
 });
 
-// mening skillarim massivini yaratish
+// mening skillarim massivi
 const mySkills = [
   { name: "HTML", level: "Intermediate", experience: 2 },
   { name: "CSS", level: "Intermediate", experience: 2 },
@@ -47,16 +47,12 @@ const mySkills = [
   { name: "Git", level: "Intermediate", experience: 2 },
 ];
 
-// htmldagi skills ro'yxatini javaScript orqali dinamik ravishda yaratish
-const skillsList = document.querySelector(".skills-list"); // skills ro'yxatini olish
-// skills ro'yxatini tozalash
-skillsList.innerHTML = ""; // eski ro'yxatni tozalash
-// har bir skill uchun li elementi yaratish va uni ro'yxatga qo'shish
-mySkills.forEach((skill) => {
-  // har bir skill uchun sikl orqali yangi li elementi yaratish
-  const li = document.createElement("li"); // yangi li elementi yaratish
-  li.textContent = `${skill.name} ${skill.level}, ${skill.experience} years`; // li elementining matnini skill ma'lumotlari bilan to'ldirish
-  skillsList.appendChild(li); // yangi li elementini ro'yxatga qo'shish, chunki appendChild() metodi yordamida yangi elementni mavjud elementning ichiga qo'shish mumkin
-});
+// mySkills massividan foydalanib htmldagi skills-list <ul> ro'yxatini <li> bilan to'ldirish
+const skillsListContainer = document.querySelector(".skills-list"); // skills-list <ul> elementini olish
 
-// practicing the git status command
+// mySkills massividan foydalanib htmldagi skills-list <ul> ro'yxatini <li> elementlari bilan to'ldirish
+mySkills.forEach((skill) => {
+  const skillItem = document.createElement("li"); // <li> elementini yaratish
+  skillItem.textContent = `${skill.name} - ${skill.level} - ${skill.experience} years`; // <li> elementining matni
+  skillsListContainer.append(skillItem); // <li> elementini skills-list <ul> ro'yxatiga qo'shish
+});
